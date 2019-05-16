2559a75e3f20fb29e1f1c218d4e5268640dcb191
Jakub Skokan <jakub.skokan@havefun.cz>
2019-05-16 11:09:28 +0200

    Release 0.13.0

--- src/Client/Authentication/Token.php
+++ src/Client/Authentication/Token.php
@@ -1,91 +1,229 @@
 <?php
 
 namespace HaveAPI\Client\Authentication;
+use HaveAPI\Client\Resource;
+use Httpful\Request;
 
 /**
  * Provider for token authentication.
  *
- * Accepts either a `token` or `username` and `password` to acquire a token.
+ * The provider can either ask for a new token or use an existing token. To
+ * ask for a new token, the provider needs to be given credentials for action
+ * `Request`. These default to `user` and `password`, but can be arbitrary.
+ *
+ * The API may require multi-step authentication, in which case the provider
+ * will invoke callback function passed via option `callback`. The callback
+ * is called with three arguments: the action name, intermediate token
+ * and an array with action input parameters and their description. The callback
+ * then returns an array with input parameter values, or string `stop` to stop
+ * the authentication process.
+ *
+ * The authentication process can be later resumed by passing option `resume`.
+ * It is an array with three keys: `action` identifying the authentication
+ * action to resume with, `token` containing the intermediate authentication
+ * token and `input` with input parameters (credentials).
+ *
  * Option `via` determines how the token is sent to the API. It defaults
  * to Token::HTTP_HEADER.
  */
 class Token extends Base {
 	const HTTP_HEADER = 0;
 	const QUERY_PARAMETER = 1;
-	
+
+	/**
+	 * @var \HaveAPI\Client\Resource
+	 */
 	private $rs;
+
 	private $configured = false;
+
 	private $token;
+
 	private $validTo = null;
+
 	private $via;
-	
+
 	/**
 	 * Request a new token if it isn't in the options.
 	 */
 	protected function setup() {
 		$this->rs = new \HaveAPI\Client\Resource($this->client, 'token', $this->description->resources->token, array());
 		$this->via = isSet($this->opts['via']) ? $this->opts['via'] : self::HTTP_HEADER;
-		
-		if(isSet($this->opts['token'])) {
+
+		if (isSet($this->opts['token'])) {
 			$this->configured = true;
 			$this->token = $this->opts['token'];
 			return;
+		} elseif (isSet($this->opts['resume'])) {
+			$r = $this->opts['resume'];
+			$this->resumeAuthentication($r['action'], $r['token'], $r['input']);
+			return;
 		}
-		
+
 		$this->requestToken();
 	}
-	
+
+	/**
+	 * Resume multi-step authentication
+	 * @param string $action
+	 * @param string $token intermediate token
+	 * @param array $input
+	 */
+	public function resumeAuthentication($action, $token, $input) {
+		$this->runAuthentication($action, array_merge(
+			$input,
+			['token' => $token]
+		));
+	}
+
 	/**
 	 * Add token header if configured. Checks token validity.
+	 * @param Request $request
 	 */
-	public function authenticate($request) {
+	public function authenticate(Request $request) {
 		if(!$this->configured)
 			return;
-		
+
 		$this->checkValidity();
-		
-		if($this->via == self::HTTP_HEADER)
+
+		if($this->via == self::HTTP_HEADER){
 			$request->addHeader($this->description->http_header, $this->token);
+		}
 	}
-	
+
 	/**
 	 * Returns token query parameter if configured.
 	 */
 	public function queryParameters() {
 		if(!$this->configured || $this->via != self::QUERY_PARAMETER)
 			return array();
-		
+
 		return array($this->description->query_parameter => $this->token);
 	}
-	
+
 	/**
 	 * Revoke the token.
 	 */
 	public function logout() {
 		$this->rs->revoke();
 	}
-	
+
 	/**
 	 * Request a new token from the API.
 	 */
 	protected function requestToken() {
-		$ret = $this->rs->request(array(
-			'login' => $this->opts['username'],
-			'password' => $this->opts['password'],
-			'lifetime' => isSet($this->opts['lifetime']) ? $this->opts['lifetime'] : 'renewable_auto',
-			'interval' => isSet($this->opts['interval']) ? $this->opts['interval'] : 300
-		));
-		
-		$this->token = $ret->getResponse()->token;
-		
-		$v = $ret->getResponse()->valid_to;
-		
-		if($v)
-			$this->validTo = strtotime($v);
-		
-		$this->configured = true;
+		$input = [
+			'lifetime' => $this->opts['lifetime'] ?? 'renewable_auto',
+			'interval' => $this->opts['interval'] ?? 300,
+		];
+
+		foreach ($this->getRequestCredentials() as $param) {
+			if (isSet($this->opts[$param]))
+				$input[$param] = $this->opts[$param];
+		}
+
+		$this->runAuthentication('request', $input);
 	}
-	
+
+	/**
+	 * Execute authentication steps until it succeeds, fails or is stopped
+	 *
+	 * The first step is executed immediately. All subsequent steps invoke the
+	 * callback to get action credentials. The callback can also decide to stop
+	 * or pause the authentication process.
+	 * @param string $action
+	 * @param array $input
+	 */
+	protected function runAuthentication($action, $input) {
+		list($cont, $nextAction, $token) = $this->authenticationStep($action, $input);
+
+		if ($cont == 'done')
+			return;
+
+		if (!isSet($this->opts['callback']) || !is_callable($this->opts['callback'])) {
+			throw new BadFunctionCallException(
+				'add callback to handle multi-step authentication'
+			);
+		}
+
+		for (;;) {
+			$cb = $this->opts['callback'](
+				$nextAction,
+				$token,
+				$this->getCustomActionCredentials($nextAction)
+			);
+
+			if ($cb === 'stop') {
+				return;
+			} elseif (!is_array($cb)) {
+				throw new RuntimeException("callback has to return an array or 'stop'");
+			}
+
+			$input = array_merge($cb, ['token' => $token]);
+
+			list($cont, $nextAction, $token) = $this->authenticationStep($nextAction, $input);
+
+			if ($cont == 'done')
+				return;
+		}
+	}
+
+	/**
+	 * Perform one authentication step and return results
+	 * @param string $action
+	 * @param array $input
+	 * @return array
+	 */
+	protected function authenticationStep($action, $input) {
+		$ret = $this->rs->{$action}($input);
+
+		if ($ret['complete']) {
+			$this->token = $ret['token'];
+
+			if($ret['valid_to'])
+				$this->validTo = strtotime($ret['valid_to']);
+
+			$this->configured = true;
+			return ['done'];
+		}
+
+		return ['continue', $ret['next_action'], $ret['token']];
+	}
+
+	/**
+	 * Return names of parameters used as credentials for action Request
+	 * @return array
+	 */
+	protected function getRequestCredentials() {
+		$ret = [];
+		$params = $this->rs->request->getParameters('input');
+
+		foreach ($params as $name => $desc) {
+			if ($name != 'lifetime' && $params != 'interval')
+				$ret[] = $name;
+		}
+
+		return $ret;
+	}
+
+	/**
+	 * Return names of parameters used as credentials for custom authentication
+	 * action
+	 * @param string action
+	 * @return array
+	 */
+	protected function getCustomActionCredentials($action) {
+		$ret = [];
+		$params = $this->rs->{$action}->getParameters('input');
+
+		foreach ($params as $name => $desc) {
+			if ($name != 'token')
+				$ret[] = $name;
+		}
+
+		return $ret;
+	}
+
 	/**
 	 * Get a new token if the current one expired.
 	 */
@@ -93,14 +231,14 @@ class Token extends Base {
 		if($this->validTo && $this->validTo < time() && isSet($this->opts['username']) && isSet($this->opts['password']))
 			$this->requestToken();
 	}
-	
+
 	/**
 	 * @return string the token
 	 */
 	public function getToken() {
 		return $this->token;
 	}
-	
+
 	/**
 	 * @return int expiration time
 	 */
--- src/Client/ResourceInstance.php
+++ src/Client/ResourceInstance.php
@@ -2,6 +2,8 @@
 
 namespace HaveAPI\Client;
 
+use HaveAPI\Client;
+
 /**
  * Resource object instance.
  */
@@ -12,7 +14,7 @@ class ResourceInstance extends Resource {
 	protected $attrs = array();
 	protected $action;
 	protected $associations = array();
-	
+
 	/**
 	 * If $response is NULL, created instance is not persistent.
 	 * @param Client $client
@@ -21,48 +23,49 @@ class ResourceInstance extends Resource {
 	 */
 	public function __construct($client, $action, $response) {
 		$r = $action->getResource();
-		
+
 		parent::__construct($client, $r->getName(), $r->getDescription(), array());
-		
+
 		$this->action = $action;
 		$this->response = $response;
-		
+
 		if($response) {
 			$this->persistent = true;
-			
+
 			if($response instanceof Response) {
 				$this->attrs = (array) $response->getResponse();
-				$this->args = $response->getMeta()->url_params;
-				
+				$meta = $response->getMeta();
+				$this->args = isset($meta->path_params) ? $meta->path_params : array();
+
 			} else {
 				$ns = $client->getSettings('meta')->{'namespace'};
-				
-				$this->args = $response->{$ns}->url_params;
+
+				$this->args = $response->{$ns}->path_params;
 // 				unset($response->{$ns});
-				
+
 				$this->attrs = (array) $response;
 			}
-			
+
 		} else {
 			$this->persistent = false;
 			$this->defineStubAttrs();
 		}
 	}
-	
+
 	/**
 	 * Do not allow creating an instance from instance.
 	 */
 	public function newInstance() {
 		throw \Exception('Cannot create a new instance from existing instance');
 	}
-	
+
 	/**
 	 * @return Response
 	 */
 	public function getApiResponse() {
 		return $this->response instanceof Response ? $this->response : null;
 	}
-	
+
 	/**
 	 * Returns all resource parameters.
 	 * @return array
@@ -70,7 +73,7 @@ class ResourceInstance extends Resource {
 	public function attributes() {
 		return $this->attrs;
 	}
-	
+
 	/**
 	 * Handle resource objecti nstance parameters. Resource parameters also have <name>_id properties,
 	 * which returns IDs without resolving the associated resource.
@@ -78,83 +81,83 @@ class ResourceInstance extends Resource {
 	 */
 	public function __get($name) {
 		$id = false;
-		
+
 		if(!array_key_exists($name, $this->attrs) && $this->endsWith($name, '_id')) {
 			$name = substr($name, 0, -3);
 			$id = true;
 		}
-		
+
 		if (!array_key_exists($name, $this->attrs))
 			return parent::__get($name);
-		
+
 		$param = $this->description->actions->{$this->action->name()}->output->parameters->{$name};
-		
+
 		switch($param->type) {
 			case 'Resource':
 				if($id)
 					return $this->attrs[$name]->{ $param->value_id };
-				
+
 				if(isSet($this->associations[$name]))
 					return $this->associations[$name];
-				
+
 				// Return resolved ResourceInstance or resolve one
 				$ns = $this->client->getSettings('meta')->{'namespace'};
-				
+
 				if ($this->attrs[$name]->{$ns}->resolved) {
 					$this->associations[$name] = new ResourceInstance(
 						$this->client,
 						$this->client[ implode('.', $param->resource) ]->show,
 						$this->attrs[$name]
 					);
-				
+
 				} else {
 					$action = $this->client[ implode('.', $param->resource) ]->show;
-					$action->applyArgs($this->attrs[$name]->{$ns}->url_params);
-					
+					$action->applyArgs($this->attrs[$name]->{$ns}->path_params);
+
 					$this->associations[$name] = $action->call();
 				}
-				
+
 				return $this->associations[$name];
-				
+
 			default:
 				return $this->attrs[$name];
 		}
 	}
-	
+
 	/**
 	 * Handle resource object instance parameters.
 	 */
 	public function __set($name, $value) {
 		$id = false;
-		
+
 		if($this->endsWith($name, '_id')) {
 			$name = substr($name, 0, -3);
 			$id = true;
 		}
-		
+
 		if(array_key_exists($name, $this->attrs)) {
 			$param = $this->description->actions->{$this->action->name()}->output->parameters->{$name};
-			
+
 			switch($param->type) {
 				case 'Resource':
 					if($id) {
 						$this->attrs[$name]->{ $param->value_id } = $value;
 						break;
 					}
-					
+
 					$this->associations[$name] = $value;
 					$this->attrs[$name]->{ $param->value_id } = $value->{ $param->value_id };
 					$this->attrs[$name]->{ $param->value_label } = $value->{ $param->value_label };
-					
+
 					break;
-					
+
 				default:
 					$this->attrs[$name] = $value;
 			}
-			
+
 			return;
 		}
-		
+
 		$trace = debug_backtrace();
 		trigger_error(
 			'Undefined property via __get(): ' . $name .
@@ -164,7 +167,7 @@ class ResourceInstance extends Resource {
 		);
         return null;
 	}
-	
+
 	/**
 	 * Create the resource object if it isn't persistent, update it if it is.
 	 */
@@ -172,23 +175,23 @@ class ResourceInstance extends Resource {
 		if($this->persistent) {
 			$action = $this->{'update'};
 			$action->directCall($this->attrsForApi($action));
-			
+
 		} else {
 			// insert
 			$action = $this->{'create'};
 			$ret = new Response($action, $action->directCall($this->attrsForApi($action)));
-			
+
 			if($ret->isOk()) {
 				$this->attrs = array_merge($this->attrs, (array) $ret->getResponse());
-				
+
 			} else {
 				throw new Exception\ActionFailed($ret, "Action '".$action->name()."' failed: ".$ret->message());
 			}
-			
+
 			$this->persistent = true;
 		}
 	}
-	
+
 	/**
 	 * Returns an array of parameters ready to be sent to the API.
 	 * @return array
@@ -196,48 +199,48 @@ class ResourceInstance extends Resource {
 	protected function attrsForApi($action) {
 		$ret = array();
 		$desc = $this->description->actions->{$action}->input->parameters;
-		
+
 		foreach($this->attrs as $k => $v) {
 			if(!isSet($desc->{$k}))
 				continue;
-			
+
 			$param = $desc->{$k};
-			
+
 			switch($param->type) {
 				case 'Resource':
 					$ret[$k] = $v->{ $param->{'value_id'} };
 					break;
-				
+
 				default:
 					$ret[$k] = $v;
 			}
 		}
-		
+
 		return $ret;
 	}
-	
+
 	/**
 	 * Create initial - NULL - resource object instance parameters.
 	 */
 	protected function defineStubAttrs() {
 		$params = $this->description->actions->{$this->action->name()}->input->parameters;
-		
+
 		foreach($params as $name => $desc) {
 			switch($desc->type) {
 				case 'Resource':
 					$c = new \stdClass();
 					$c->{$desc->value_id} = null;
 					$c->{$desc->value_label} = null;
-					
+
 					$this->attrs[$name] = $c;
 					break;
-				
+
 				default:
 					$this->attrs[$name] = null;
 			}
 		}
 	}
-	
+
 	/**
 	 * Return true if $str ends with $ending.
 	 * @return boolean
